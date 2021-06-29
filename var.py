8136b97e02811f560285309c552f00ce90fe5167
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-06-29 17:42:33 +0530

    Update var.py

--- var.py
+++ var.py
@@ -3,7 +3,7 @@ import os
 
 ENV = bool(os.environ.get("ENV", False))
 if ENV:
-    from heroku_config import Var as Config
+    from exampleconfig import Var as Config
 elif os.path.exists("config.py"):
     from config import Development as Config  # noqa
 
